359cfd5b37e89f7626f4e7c608d6cfc0b07b2f68
hermani456 <hermani457@gmail.com>
2024-11-13 01:42:32 -0300

    rename profile screen file, add onboarding screen, and update app entry point

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function ProfileScreen() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl">Profile</Text>
+      <Text className="text-6xl font-bold bg-gradient-to-b p-2 from-teal-400 to-purple-700 text-transparent bg-clip-text">
+        Profile Screen
+      </Text>
     </View>
   );
 }
